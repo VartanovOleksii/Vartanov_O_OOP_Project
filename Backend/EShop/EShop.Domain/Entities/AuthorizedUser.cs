27cf5293a93483bfb6e912d26981728a69856088
AVir3307 <lexvartanov@gmail.com>
2026-06-16 18:37:22 +0200

    Add AuthorizedUser class

---
+++ Backend/EShop/EShop.Domain/Entities/AuthorizedUser.cs
@@ -0,0 +1,13 @@
+﻿namespace EShop.Domain.Entities;
+
+public abstract class AuthorizedUser : Entity
+{
+    public string UserId { get; protected set; } = string.Empty;
+
+    public string UserAddress { get; protected set; } = string.Empty;
+
+    public override bool IsValid()
+    {
+        throw new NotImplementedException();
+    }
+}
