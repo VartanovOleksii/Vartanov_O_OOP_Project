4b8d345512c01398c3cd3c39f9e743cc60a00543
AVir3307 <lexvartanov@gmail.com>
2026-06-16 22:49:18 +0200

    Implement JsonDataPort

--- Backend/EShop/EShop.Domain/Ports/JsonDataPort.cs
+++ Backend/EShop/EShop.Domain/Ports/JsonDataPort.cs
@@ -1,14 +1,25 @@
+using Microsoft.Extensions.Hosting;
+using System.Text.Json;
+
 namespace EShop.Domain.Ports;
 
 public class JsonDataPort<T>
 {
     public string Export(IEnumerable<T> collection)
     {
-        throw new NotImplementedException();
+        if (collection == null)
+            return "[]";
+
+        string json = System.Text.Json.JsonSerializer.Serialize(collection);
+
+        return json;
     }
 
     public IEnumerable<T> Import(string json)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrEmpty(json))
+            return Enumerable.Empty<T>();
+
+        return JsonSerializer.Deserialize<List<T>>(json) ?? [];
     }
 }
