b777946f55d3997c39c78eaf7e1880b12e50d06e
AVir3307 <lexvartanov@gmail.com>
2026-06-16 18:51:28 +0200

    Add CartItem class

---
+++ Backend/EShop/EShop.Domain/Entities/CartItem.cs
@@ -0,0 +1,17 @@
+﻿namespace EShop.Domain.Entities;
+
+public class CartItem : Entity
+{
+    public int BuyerId { get; private set; }
+    public Buyer? CartBuyer { get; private set; }
+
+    public int ProductId { get; private set; }
+    public Product? CartProduct { get; private set; }
+
+    public int CartQuantity { get; private set; }
+
+    public override bool IsValid()
+    {
+        throw new NotImplementedException();
+    }
+}
