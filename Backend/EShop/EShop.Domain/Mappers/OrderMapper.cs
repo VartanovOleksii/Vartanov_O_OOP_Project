2787a4ba06b7babe02b1aa2a6912784571d7e766
AVir3307 <lexvartanov@gmail.com>
2026-06-16 20:17:12 +0200

    Add Order and Product mappers

---
+++ Backend/EShop/EShop.Domain/Mappers/OrderMapper.cs
@@ -0,0 +1,17 @@
+using EShop.Domain.DTOs;
+using EShop.Domain.Entities;
+
+namespace EShop.Domain.Mappers;
+
+public static class OrderMapper
+{
+    public static OrderDto ToDto(Order order)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static Order ToEntity(OrderDto dto)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Backend/EShop/EShop.Domain/Mappers/ProductMapper.cs
@@ -0,0 +1,17 @@
+using EShop.Domain.DTOs;
+using EShop.Domain.Entities;
+
+namespace EShop.Domain.Mappers;
+
+public static class ProductMapper
+{
+    public static ProductDto ToDto(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static Product ToEntity(ProductDto dto)
+    {
+        throw new NotImplementedException();
+    }
+}
