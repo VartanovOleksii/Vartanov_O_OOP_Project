2d5a3d28d197d028f755ca6ca931ae706ea4c914
AVir3307 <lexvartanov@gmail.com>
2026-06-16 18:53:26 +0200

    Add ProductEventArgs event

---
+++ Backend/EShop/EShop.Domain/Events/ProductEventArgs.cs
@@ -0,0 +1,17 @@
+﻿namespace EShop.Domain.Events;
+
+public class ProductEventArgs : EventArgs
+{
+    public int ProductId { get; }
+
+    public int OldQuantity { get; }
+
+    public int NewQuantity { get; }
+
+    public ProductEventArgs(int productId, int oldQuantity, int newQuantity)
+    {
+        ProductId = productId;
+        OldQuantity = oldQuantity;
+        NewQuantity = newQuantity;
+    }
+}
