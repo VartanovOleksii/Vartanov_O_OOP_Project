1523de404cc9f3b5e87398fab4509f2642eb386c
AVir3307 <lexvartanov@gmail.com>
2026-06-16 18:31:52 +0200

    Add Entity class

---
+++ Backend/EShop/EShop.Domain/Entities/Entity.cs
@@ -0,0 +1,20 @@
+﻿namespace EShop.Domain.Entities;
+
+public abstract class Entity : IEquatable<Entity>
+{
+    public int Id { get; protected set; }
+
+    public abstract bool IsValid();
+
+    
+    public bool Equals(Entity? other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool Equals(object? obj)
+    {
+        throw new NotImplementedException();
+    }
+
+}
