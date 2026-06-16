bdc66a82a066ec14acf67e86662ff6dc61ef1468
AVir3307 <lexvartanov@gmail.com>
2026-06-16 18:43:57 +0200

    Add Product and ProductImage classes

---
+++ Backend/EShop/EShop.Domain/Entities/Product.cs
@@ -0,0 +1,46 @@
+﻿using EShop.Domain.Events;
+
+namespace EShop.Domain.Entities;
+
+public class Product : Entity, IComparable<Product>
+{
+    public string ProductName { get; private set; } = string.Empty;
+
+    public string ProductDescription { get; private set; } = string.Empty;
+
+    public decimal ProductPrice { get; private set; }
+
+    public int ProductQuantity { get; private set; }
+    public int SellerId { get; private set; }
+
+    public Seller? ProductSeller { get; private set; }
+
+    public List<ProductImage> ProductImages { get; private set; } = [];
+
+    public event EventHandler<ProductEventArgs>? StockChanged;
+
+    public void IncreaseQuantity(int amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DecreaseQuantity(int amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddImage(ProductImage image)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int CompareTo(Product? other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool IsValid()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Backend/EShop/EShop.Domain/Entities/ProductImage.cs
@@ -0,0 +1,16 @@
+﻿namespace EShop.Domain.Entities;
+
+public class ProductImage : Entity
+{
+    public int ProductId { get; private set; }
+    public Product? ImageProduct { get; private set; }
+
+    public string ImagePath { get; private set; } = string.Empty;
+
+    public string? ImageAltText { get; private set; }
+
+    public override bool IsValid()
+    {
+        throw new NotImplementedException();
+    }
+}
